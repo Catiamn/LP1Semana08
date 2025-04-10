4df83c1e50130d1bdf0f441efc35a20f7fdc1d43
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-04-10 11:53:38 +0100

    Modified Unit

--- GameUnit/Unit.cs
+++ GameUnit/Unit.cs
@@ -3,12 +3,38 @@ namespace GameUnit
     public class Unit
     {
         private readonly int movement;
-        public int Health { get; set; }
+        public virtual int Health { get; set; }
 
         public Unit(int movement, int health)
         {
             this.movement = movement;
             Health = health;
         }
+
+        public class Move()
+        {
+            int casas =+ movimento
+
+            public static List<string> romanNumerals = new List<string>() { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            public static List<int> numerals = new List<int>() { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+            public static string ToRomanNumeral(int number)
+            {
+                var romanNumeral = string.Empty;
+                while (number > 0)
+                {
+                    // find biggest numeral that is less than equal to number
+                    var index = numerals.FindIndex(x => x <= number);
+                    // subtract it's value from your number
+                    number -= numerals[index];
+                    // tack it onto the end of your roman numeral
+                    romanNumeral += romanNumerals[index];
+                }
+                return romanNumeral;
+                ToRomanNumeral(casas)
+            }
+        }
+
+        public abstract float Cost{}
     }
 }
